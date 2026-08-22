647f2cc2c1eb777a8b8ff09d16fef3c2ea0a458e
Usman <baumonmt786@gmail.com>
2026-08-22 16:05:23 +0530

    Add REST framework integration with serializers and API views for menu and booking

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,20 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
+booking_router = routers.DefaultRouter()
+booking_router.register(r'tables', views.BookingViewSet)
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view(), name='menu-items'),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu-item'),
+    path('booking/', include(booking_router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+urlpatterns += router.urls
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer, UserSerializer
 
 def sayHello(request):
 	return HttpResponse('Hello World')
@@ -8,3 +13,24 @@ def sayHello(request):
 
 def index(request):
 	return render(request, 'index.html', {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+	queryset = Menu.objects.all()
+	serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+	queryset = Menu.objects.all()
+	serializer_class = MenuSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+	permission_classes = [IsAuthenticated]
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+	queryset = Booking.objects.all()
+	serializer_class = BookingSerializer
