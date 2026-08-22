205e79547d7e7fe49d710f59ce7b029171409e64
Usman <baumonmt786@gmail.com>
2026-08-22 15:57:13 +0530

    Update database configuration to use MySQL and add Booking and Menu models with initial migration

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': 'usman@786',
+        'HOST': '127.0.0.1',
+        'PORT': '3310',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
